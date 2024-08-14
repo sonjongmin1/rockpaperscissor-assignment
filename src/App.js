227a211d91dbefb2a88d8981debd66e27dd4a714
jong_min <whdals0525@gmail.com>
2024-08-15 00:24:43 +0900

    rockpaperscissor-assignment-3

--- src/App.js
+++ src/App.js
@@ -2,52 +2,63 @@ import { useState } from 'react'
 import './App.css'
 import Box from './component/Box'
 
-//1. 박스 2개 (타이틀, 사진, 결과)
-//2, 가위, 바위, 보 버튼이 있다.
-//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-//4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
-//5. 3,4의 결과를 가지고 누가이겼는지 승패를 따진다.
-//6. 승패 결과의 따라 테두리 색이 바뀐다.
-
 const choice = {
   rock: {
     name: 'Rock',
-    img: 'https://img.freepik.com/free-psd/hand-drawn-rock-formation_23-2151544043.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721001600&semt=ais_user',
+    img: 'http://www.hallymmedialab.com/news/photo/202004/874_1627_4638.jpg',
   },
-
-  scissor: {
-    name: 'Scissor',
-    img: 'https://cdn-icons-png.flaticon.com/512/5418/5418004.png',
+  scissors: {
+    name: 'Scissors',
+    img: 'https://cdn-icons-png.flaticon.com/512/4975/4975327.png',
   },
-
   paper: {
     name: 'Paper',
-    img: 'https://thumb.ac-illust.com/ec/ec8f375e112ebdd87223a2fc88d19235_t.jpeg',
+    img: 'https://img.freepik.com/free-vector/white-empty-paper-sheet-with-curl_1284-43065.jpg',
   },
 }
 
 function App() {
   const [userSelect, setUserSelect] = useState(null)
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState('')
+  const [pcResult, pcSetResult] = useState('')
+
   const play = (userChoice) => {
     setUserSelect(choice[userChoice])
+    let computerChoice = randomChoice()
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice))
   }
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return 'tie'
+    } else if (user.name === 'Rock')
+      return computer.name === 'Scissors' ? 'win' : 'lose'
+    else if (user.name === 'Scissors')
+      return computer.name === 'Paper' ? 'win' : 'lose'
+    else if (user.name === 'Paper')
+      return computer.name === 'Rock' ? 'win' : 'lose'
+  }
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice)
+    let randomItem = Math.floor(Math.random() * itemArray.length)
+    let final = itemArray[randomItem]
+    return choice[final]
+  }
+
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect} />
-        <Box title="Computer" item={userSelect} />
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
 
       <div className="main">
-        <button variant="primary" onClick={() => play('scissor')}>
-          가위
-        </button>
-        <button variant="primary" onClick={() => play('rock')}>
-          바위
-        </button>
-        <button variant="primary" onClick={() => play('paper')}>
-          보
-        </button>
+        <button onClick={() => play('scissors')}>Scissors</button>
+        <button onClick={() => play('rock')}>Rock</button>
+        <button onClick={() => play('paper')}>Paper</button>
       </div>
     </div>
   )
